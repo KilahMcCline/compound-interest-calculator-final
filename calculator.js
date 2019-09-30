47f6e5465de4784f514eaadd6b2a7adbc2453d88
Kilah McCline <54633560+KilahMcCline@users.noreply.github.com>
2019-09-30 11:16:15 -0500

    Final Calculator Updates

--- calculator.js
+++ calculator.js
@@ -1,4 +1,7 @@
-// Input Elements
+/*******************************************************************************
+Elements
+*******************************************************************************/
+
 const amountEl = document.getElementsByName('amount');
 const contributionEl = document.getElementsByName('contribution');
 const durationEl = document.getElementsByName('duration');
@@ -8,20 +11,126 @@ const interestEl = document.getElementsByName('interest');
 const startingVal = document.querySelectorAll('.starting-val');
 const contributionVal = document.querySelectorAll('.contribution-val');
 const durationVal = document.querySelectorAll('.duration-val');
+const returnVal = document.querySelectorAll('.return-val');
 const finalVal = document.querySelectorAll('.final-val');
 
-// Format Value
-const formatValue = (value) => parseFloat(Math.round(value * 100) / 100).toFixed(2);
 
-// Display Results
-const displayResults = (amount, duration, totalContribution, totalReturn, finalBalance) => {
-	startingVal[0].innerHTML = '$' + amount;
-	contributionVal[0].innerHTML = '$' + totalContribution;
-	durationVal[0].innerHTML = duration;
-	finalVal[0].innerHTML = '$' + finalBalance;
+/*******************************************************************************
+Format Value
+*******************************************************************************/
+
+const formatValue = (value, type) => {
+	var formatted = parseFloat(Math.round(value * 100) / 100).toFixed(2);
+
+	switch (type) {
+		case '$':
+			var x = formatted.split('.'),
+				x1 = x[0],
+				x2 = x.length > 1 ? '.' + x[1] : '',
+				rgx = /(\d+)(\d{3})/;
+
+			// Add comma
+			while (rgx.test(x1)) { x1 = x1.replace(rgx, '$1' + ',' + '$2') }
+			return '$' + x1 + x2;
+			break;
+		case '%':
+			return formatted + '%';
+			break;
+		default:
+			return formatted
+			break;
+	}
 }
 
-// Calculate
+
+/*******************************************************************************
+Display Results
+*******************************************************************************/
+
+const displayResults = (amount, duration, totalContribution, totalReturn, finalBalance) => {
+	startingVal[0].innerHTML = amount;
+	contributionVal[0].innerHTML = totalContribution;
+	durationVal[0].innerHTML = duration;
+	// returnVal[0].innerHTML = totalReturn;
+	finalVal[0].innerHTML = finalBalance;
+}
+
+
+/*******************************************************************************
+Get Ratio
+*******************************************************************************/
+
+const getRatio = (value, max) => {
+	return 100 * (value/max);
+}
+
+
+/*******************************************************************************
+Pie Chart
+*******************************************************************************/
+
+var pieCanvas = document.getElementById('piechart').getContext('2d');
+var pieChart = new Chart(pieCanvas, {
+	// The type of chart we want to create
+	type: 'pie',
+
+	// The data for our dataset
+	data : {
+		datasets: [{
+			data: [10, 20, 30],
+			backgroundColor: ['#4654A4','#1984CC','#70C7AD'],
+		}],
+
+		// These labels appear in the legend and in the tooltips when hovering different arcs
+		labels: [
+		'Starting Amount',
+		'Contribution Total',
+		'Return Total'
+		]
+	},
+
+	// Configuration options go here
+	options: {
+		labeles: {
+			fontSyle:'Georgia',
+			fontSize:30,
+		}
+
+
+
+	}
+
+
+});
+
+
+const updatePieChart = (amount, totalContribution, totalReturn) => {
+	pieChart.data.datasets[0].data = [+amount, +totalContribution, +totalReturn];
+	pieChart.update();
+}
+
+
+/*******************************************************************************
+Render Visuals
+*******************************************************************************/
+
+const renderVisuals = (amount, totalContribution, totalReturn, finalBalance) => {
+	const startingBar = document.querySelectorAll('.starting-bar');
+	const contributionBar = document.querySelectorAll('.contribution-bar');
+	const returnBar = document.querySelectorAll('.return-bar');
+	const finalBar = document.querySelectorAll('.final-bar');
+
+	// startingBar[0].style.width = getRatio(amount, finalBalance) + '%';
+	// contributionBar[0].style.width = getRatio(totalContribution, finalBalance) + '%';
+	// returnBar[0].style.width = getRatio(totalReturn, finalBalance) + '%';
+	// finalBar[0].style.width = getRatio(finalBalance, finalBalance) + '%';
+}
+
+
+/*******************************************************************************
+Calculate
+*******************************************************************************/
+
 const calculate = () => {
 	let amount = amountEl[0].value;
 	let contribution = contributionEl[0].value;
@@ -30,17 +139,23 @@ const calculate = () => {
 	let rate = interest/100;
 	let finalBalance;
 	let totalContribution;
-	let totalReturn; }
-
-
-
+	let totalReturn;
 
 	// const
-	finalBalance = amount * Math.pow(1 + rate, duration) + contribution * ( (Math.pow(1 + rate, duration) - 1) / rate );
-	totalContribution = contribution * duration;
-	totalReturn = finalBalance - amount - totalContribution;
+	if (rate) {
+		finalBalance = amount * Math.pow(1 + rate, duration) + contribution * ( (Math.pow(1 + rate, duration) - 1) / rate );
+		totalContribution = contribution * duration;
+		totalReturn = finalBalance - amount - totalContribution;
+	} else {
+		totalContribution = contribution * duration;
+		finalBalance = +amount + totalContribution;
+		totalReturn = finalBalance - amount - totalContribution;
+	}
 
-	displayResults(amount, duration, formatValue(totalContribution), formatValue(totalReturn), formatValue(finalBalance));
-
+	renderVisuals(amount, totalContribution, totalReturn, finalBalance);
+	displayResults(formatValue(amount, '$'), duration, formatValue(totalContribution, '$'), formatValue(totalReturn, '$'), formatValue(finalBalance, '$'));
+	updatePieChart(amount, totalContribution, totalReturn);
 }
+
+// On Load
 calculate();
